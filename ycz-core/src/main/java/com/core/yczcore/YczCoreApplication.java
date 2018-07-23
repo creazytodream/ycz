3a11a66ae91c72e6f2c28a2317b11ab7cc80aeae
yumingbin <1076950558@qq.com>
2018-07-23 14:51:06 +0800

    no message

---
+++ ycz-core/src/main/java/com/core/yczcore/YczCoreApplication.java
@@ -0,0 +1,12 @@
+package com.core.yczcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YczCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YczCoreApplication.class, args);
+    }
+}
---
+++ ycz-service/src/main/java/com/ycz/yczservice/YczServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ycz.yczservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YczServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YczServiceApplication.class, args);
+    }
+}
